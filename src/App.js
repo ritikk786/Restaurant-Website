c5dd6366c29ec5281b9f520f42e53ce50e1edf0f
ritikk786 <kumarritik9540@gmail.com>
2023-04-26 13:51:04 +0530

    Header

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Header from "./Components/Layout/Header";
+function App() {
+  return (
+    <div>
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,19 @@
+import React,{Fragment} from 'react'
+import classes from './Header.module.css'
+import HeaderImage from '../../assets/meals.jpg'
+import HeaderCartButton from './HeaderCartButton'
+
+const Header =()=>{
+    return(
+       <Fragment>
+        <header className={classes.header}>
+            <h1>React Meals</h1>
+            <HeaderCartButton/>
+        </header>
+        <div className={classes['main-image']}>
+        <img src={HeaderImage} alt='meals '/>
+        </div>
+       </Fragment>
+    )
+}
+export default Header;
